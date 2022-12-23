8eb2ca54c5ed0c5622edc8014cc2eafd62f42550
unknown <withbbang@gmail.com>
2022-12-23 17:05:11 +0900

    img, video 태그 모두 추가

--- public/script.js
+++ public/script.js
@@ -9,6 +9,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   handleDrawDetections();
 });
 
+// 훈련 모델 가져오기
 const handleGetModels = async () => {
   const MODEL_URL = "/models";
 
@@ -17,20 +18,25 @@ const handleGetModels = async () => {
   await faceapi.loadFaceRecognitionModel(MODEL_URL);
 };
 
+// 얼굴 인식 정보 처리하기
 const handleGetFaceDescriptions = async () => {
-  const input = document.getElementById("video");
+  const img = document.getElementById("img");
+  const video = document.getElementById("video");
+
   fullFaceDescriptions = await faceapi
-    .detectAllFaces(input)
+    .detectAllFaces(video)
     .withFaceLandmarks()
     .withFaceDescriptors();
 };
 
+// 디스플레이 사이즈 조정시 얼굴 박스 빛 윤곽 조정
 const handleCorrespondImage = () => {
   fullFaceDescriptions = faceapi.resizeResults(fullFaceDescriptions);
 };
 
+// 얼굴 박스 및 윤곽 그리기
 const handleDrawDetections = () => {
-  //   const canvas = faceapi.createCanvasFromMedia(document.getElementById("img"));
+  const img = faceapi.createCanvasFromMedia(document.getElementById("img"));
   const video = faceapi.createCanvasFromMedia(document.getElementById("video"));
   const div = document.getElementById("div");
   div.append(video);
